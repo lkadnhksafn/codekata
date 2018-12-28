bc9da58d1a0f94576b9226455046e816f719e4e2
lkadnhksafn <46000553+lkadnhksafn@users.noreply.github.com>
2018-12-27 22:57:02 -0800

    to check pos,neg,zero

--- posneg.java
+++ posneg.java
@@ -7,8 +7,11 @@ public class vowels {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		int num=sc.nextInt();
-		if(num>=0) {
+		if(num>0) {
 			System.out.println("Postive");
+		}
+		else if(num==0){
+			System.out.println("Zero");
 		}
 		else {
 			System.out.println("Negative");
