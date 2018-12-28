e1326e73c31cd0b9ace5e61017dc7bdd822ba669
lkadnhksafn <46000553+lkadnhksafn@users.noreply.github.com>
2018-12-27 22:53:46 -0800

    to check positive  or negativee

---
+++ posneg.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num>=0) {
+			System.out.println("Postive");
+		}
+		else {
+			System.out.println("Negative");
+		}
+		
+
+	}
+
+}
